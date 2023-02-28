7b7c52b97fcb3de54dfe9ace881ee09c015203a2
laijd <3214719632@qq.com>
2023-02-28 16:37:24 +0800

    首页删除多余功能

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -14,23 +14,7 @@
                         上次登录时间：
                         <span>2019-11-01</span>
                     </div>
-<!--                    <div class="user-info-list">-->
-<!--                        上次登录地点：-->
-<!--                        <span>东莞</span>-->
-<!--                    </div>-->
                 </el-card>
-<!--                <el-card shadow="hover" style="height:252px;">-->
-<!--                    <template #header>-->
-<!--                        <div class="clearfix">-->
-<!--                            <span>语言详情</span>-->
-<!--                        </div>-->
-<!--                    </template>-->
-<!--                    Vue-->
-<!--                    <el-progress :percentage="71.3" color="#42b983"></el-progress>JavaScript-->
-<!--                    <el-progress :percentage="24.1" color="#f1e05a"></el-progress>CSS-->
-<!--                    <el-progress :percentage="13.7"></el-progress>HTML-->
-<!--                    <el-progress :percentage="5.9" color="#f56c6c"></el-progress>-->
-<!--                </el-card>-->
             </el-col>
             <el-col :span="16">
                 <el-row :gutter="20" class="mgb20">
